b0a343657d7d28068c6f8b7e8ab9991f06d87f43
celestialaxis <113936480+celestialaxis@users.noreply.github.com>
2022-11-20 22:19:24 +0700

    update

--- 024. Linear Combination Algebraic Form - Linear Algebra Game Jam/Modules/Characters/Player/PlayerKinematic.gd
+++ 024. Linear Combination Algebraic Form - Linear Algebra Game Jam/Modules/Characters/Player/PlayerKinematic.gd
@@ -5,22 +5,13 @@ const vector_j: Vector2 = Vector2.DOWN;
 
 var mouse_position: Vector2 = Vector2();
 var difference_vector: Vector2 = Vector2();
-var difference_lerp: Vector2 = self.difference_vector;
 
 func _process(delta: float) -> void:
-	self.get_input();
+	self.get_input(delta);
 
-func _physics_process(delta: float) -> void:
-	if(self.difference_lerp.length() >= self.difference_vector.length() - 2):
-		self.difference_vector = Vector2.ZERO;
-	self.difference_lerp = self.difference_lerp.linear_interpolate(self.difference_vector, 0.3) * delta;
-	print(" - Lerp - ");
-	print(self.difference_lerp);
-	print(" - Vector - ");
-	print(self.difference_vector)
-
-func get_input() -> void:
+func get_input(delta) -> void:
 	if(Input.is_action_just_pressed("left_click")):
 		self.mouse_position = self.get_viewport().get_mouse_position();
 		self.difference_vector = self.mouse_position - self.position;
-	self.position += (vector_i * self.difference_lerp) + (vector_j * self.difference_lerp);
+		yield(self.create_tween().tween_property(self, "position", self.position + self.vector_i * self.difference_vector, 0.3), "finished");
+		yield(self.create_tween().tween_property(self, "position", self.position + self.vector_j * self.difference_vector, 0.3), "finished");

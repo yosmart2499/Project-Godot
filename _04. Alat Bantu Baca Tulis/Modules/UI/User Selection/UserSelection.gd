c074db20312898db02f0028add9d31c62e2dc15e
celestialaxis <113936480+yosmart2499@users.noreply.github.com>
2023-01-18 11:21:19 +0700

    Change UI

--- _04. Alat Bantu Baca Tulis/Modules/UI/User Selection/UserSelection.gd
+++ _04. Alat Bantu Baca Tulis/Modules/UI/User Selection/UserSelection.gd
@@ -1,11 +1,10 @@
 extends Control
 
-func _on_StudentBtn_pressed():
+func _on_StudentBtn_pressed() -> void:
 	UserAccess.set_user(UserAccess.Category.STUDENT);
 	UserAccess.set_screen(UserAccess.Scene.CHOOSE_FEATURE);
 
 
-func _on_TeacherBtn_pressed():
+func _on_TeacherBtn_pressed() -> void:
 	UserAccess.set_user(UserAccess.Category.TEACHER);
 	UserAccess.set_screen(UserAccess.Scene.CHOOSE_FEATURE);
-	
